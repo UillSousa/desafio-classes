c53145fb216938a7be2c2eb7ee69f2e8006f7572
UillSousa <uilassousa@yahoo.com.br>
2024-07-30 13:14:28 -0300

    Desafio Concluido

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 class hero{
-    constructor(){
+    constructor(nome, idade, tipo){
         this.nome = nome 
         this.idade = idade
         this.tipo = tipo
     }
 
+    
     atacar(){
-        console.log(`o ${tipo} atacou usando ${ataque}`)
+        let ataque;
+
+        switch (this.tipo) {
+            case "Mago":
+              ataque = "Magia"
+              break;
+            case "Guerreiro":
+              ataque = "Espada"
+              break;
+            case "Monge":
+              ataque = "Artes Marciais"
+              break;
+            case "Ninja":
+              ataque = "Shuriken"
+              break
+            default:
+              ataque = "Ataque Desconhecido"
+          }
+          
+        console.log(`O ${this.tipo} atacou usando ${ataque}`)
     }
 
-    if(nome = "mago"){
-        ataque = "usou magia"
-    }
+}
 
-} 
+let hero1 = new hero("Uilas", 25, "Mago") 
+let hero2 = new hero("Iago", 27, "Guerreiro")
+
+hero1.atacar()
+hero2.atacar()
+
