aeae363afb3c1926b1e00da1b8d6d9a3e1c00f2f
UillSousa <uilassousa@yahoo.com.br>
2024-07-30 11:10:17 -0300

    changes

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 class hero{
+    constructor(){
+        this.nome = nome 
+        this.idade = idade
+        this.tipo = tipo
+    }
 
+    atacar(){
+        console.log(`o ${tipo} atacou usando ${ataque}`)
+    }
+
+    if(nome = "mago"){
+        ataque = "usou magia"
+    }
 
 } 
